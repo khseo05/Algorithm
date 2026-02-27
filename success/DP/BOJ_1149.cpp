31dc59b315fc1a1e403e00f3a6ae4a3f5052f267
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-02-27 11:48:09 +0000

    success / BOJ_1149

--- success/DP/BOJ_1149.cpp
+++ success/DP/BOJ_1149.cpp
@@ -1,21 +1,34 @@
+// 2026-02-22
+// 2026-02-27
 #include <iostream>
 #include <vector>
-#include <climits>
+#include <algorithm>
 using namespace std;
 
-int memo(int n, vector<vector<int>>& v, vector<vector<int>>& memo) {
+int dp(int n, vector<vector<int>> v, vector<vector<int>> memo) {
+
+    memo[0][0] = v[0][0];
+    memo[0][1] = v[0][1];
+    memo[0][2] = v[0][2];
+
     for (int i=1; i<n; i++) {
         memo[i][0] = v[i][0] + min(memo[i-1][1], memo[i-1][2]);
         memo[i][1] = v[i][1] + min(memo[i-1][0], memo[i-1][2]);
         memo[i][2] = v[i][2] + min(memo[i-1][0], memo[i-1][1]);
     }
+
+    return min(memo[n-1][0], min(memo[n-1][1], memo[n-1][2]));
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n;
     cin >> n;
 
-    vector<vector<int>> v(n, vector<int>(3,0));
+    vector<vector<int>> v(n, vector<int>(3, 0));
+
     for (int i=0; i<n; i++) {
         for (int j=0; j<3; j++) {
             int a;
@@ -24,11 +37,6 @@ int main() {
         }
     }
 
-    vector<vector<int>> memo(n, vector<int>(3,-1));
-    memo[0][0] = v[0][0];
-    memo[0][1] = v[0][1];
-    memo[0][2] = v[0][2];
-
-    memo(n, v, memo);
-    cout << min({memo[n-1][0], memo[n-1][1], memo[n-1][2]});
+    vector<vector<int>> memo(n, vector<int>(3, 0));
+    cout << dp(n, v, memo);
 }
