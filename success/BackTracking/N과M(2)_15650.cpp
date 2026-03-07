4df840b6c4c12625ea0cfa39f2365e8a72835ff4
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-03-07 06:28:48 +0000

    success / N과M(2)_15650

---
+++ success/BackTracking/N과M(2)_15650.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void dp(int n, int m, int start, int depth, vector<int>& v) {
+    if (depth == m) {
+        for (int i=0; i<m; i++) {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i=start; i<=n; i++) {
+        v[depth] = i;
+        dp(n, m , i+1, depth+1, v);
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> v(n+1);
+    dp(n, m, 1, 0, v);
+}
