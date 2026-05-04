e9c6e29c0eca10b1add0f177c9bf9ecab6882c9d
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-05-04 11:02:20 +0000

    fail / 메뉴리뉴얼

---
+++ programmers/fail/메뉴리뉴얼.cpp
@@ -0,0 +1,10 @@
+//2026.05.04 fail
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> orders, vector<int> course) {
+    vector<string> answer;
+    return answer;
+}
