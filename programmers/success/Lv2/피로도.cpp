21932f2f590ed97c80677a775dd27a9b6f915c19
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-05-09 06:27:33 +0000

    success / 피로도

--- programmers/success/Lv2/피로도.cpp
+++ programmers/success/Lv2/피로도.cpp
@@ -1,29 +1,31 @@
 //2026.05.02 success
+//2026.05.09 success
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 
-int dfs(int k, vector<vector<int>>& dungeons, vector<bool>& visited, int count) {
-    for (int i=0; i<dungeons.size(); i++) {
-        if (visited[i]) continue;
+int answer = -1;
+unordered_map<int, bool> m;
 
+void dfs(int& k, vector<vector<int>> dungeons, int cnt, int ans) {
+    answer = max(answer, ans);
+
+    for (int i=0; i<dungeons.size(); i++) {
         if (k >= dungeons[i][0]) {
-            visited[i] = true;
-            count = max(dfs(k - dungeons[i][1], dungeons, visited, count+1), count);
-            visited[i] = false;
+            if (m[i]) continue;
+            m[i] = true;
+            k -= dungeons[i][1];
+            dfs(k, dungeons, cnt+1, ans+1);
+            k += dungeons[i][1];
+            m[i] = false;
         }
     }
-
-    return count;
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-
-    int answer = 0;
-    vector<bool> visited(dungeons.size(), false);
-    answer = dfs(k, dungeons, visited, 0);
-
+    dfs(k, dungeons, 0, 0);
     return answer;
 }
