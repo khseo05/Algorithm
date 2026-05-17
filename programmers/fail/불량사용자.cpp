403d04bb11b4b90602bb008d7b6639184fafdc1c
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-05-17 08:20:15 +0000

    fail / 불량사용자

---
+++ programmers/fail/불량사용자.cpp
@@ -0,0 +1,38 @@
+//2026.05.15 fail
+//2026.05.17 fail
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+unordered_set<int> answer;
+
+bool match(string user, string ban) {
+    if (user.length() != ban.length()) return false;
+
+    for (int i=0; i<user.length(); i++) {
+        if (ban[i] != '*' && user[i] != ban[i]) return false;
+    }
+
+    return true;
+}
+
+void dfs(vector<string>& user_id, vector<string>& banned_id, int idx, int mask) {
+    if (idx == banned_id.size()) {
+        answer.insert(mask);
+        return;
+    }
+
+    for (int i=0; i<user_id.size(); i++) {
+        if (mask & (1 << i)) continue;
+        if (!match(user_id[i], banned_id[idx])) continue;
+
+        dfs(user_id, banned_id, idx+1, mask | (1 << i));
+    }
+}
+
+int solution(vector<string> user_id, vector<string> banned_id) {
+    dfs(user_id, banned_id, 0, 0);
+    return answer.size();
+}
