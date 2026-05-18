60f952769f522f8992f3cb8fc7a9f3d711a2e755
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-05-18 11:37:49 +0000

    success / 후보키

--- programmers/success/Lv2/후보키.cpp
+++ programmers/success/Lv2/후보키.cpp
@@ -1,47 +1,45 @@
 //2026.05.05 fail
 //2026.05.11 fail
 //2026.05.16 success
+//2026.05.18 success
 #include <string>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
 
-bool isUnique(vector<vector<string>> relation, int mask) {
-    unordered_map<string, bool> m;
-
-    for (int i=0; i<relation.size(); i++) {
-        string str;
-        for (int j=0; j<relation[0].size(); j++) {
-            if (!(mask & (1 << j))) continue;
-            str += relation[i][j];
-        }
-        if (m[str]) return false;
-        m[str] = true;
-    }
-
-    return true;
-}
-
 int solution(vector<vector<string>> relation) {
-    int size = relation.size();
-
     vector<int> answer;
 
     for (int mask = 1; mask < (1 << relation[0].size()); mask++) {
-        bool min = true;
+        unordered_map<string, bool> m;
+        bool isSubset = false;
         for (int ans : answer) {
             if ((mask & ans) == ans) {
-                min = false;
+                isSubset = true;
                 break;
             }
         }
 
-        if (!min) continue;
+        if (isSubset) continue;
 
-        if (isUnique(relation, mask)) {
-            answer.push_back(mask);
+        bool check = true;
+        for (int i = 0; i < relation.size(); i++) {
+            string s;
+            for (int j = 0; j <relation[0].size(); j++) {
+                if (mask & (1 << j)) {
+                    s += relation[i][j];
+                }
+            }
+
+            if (m[s]) {
+                check = false;
+                break;
+            }
+            m[s] = true;
         }
+
+        if (check) answer.push_back(mask);
     }
 
     return answer.size();
