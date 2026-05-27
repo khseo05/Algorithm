d2b81bfa3d4c1f63dad756a23b187e4a94415179
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-05-27 06:18:54 +0000

    success / 전화번호목록

--- programmers/success/Lv2/전화번호목록.cpp
+++ programmers/success/Lv2/전화번호목록.cpp
@@ -8,10 +8,10 @@ using namespace std;
 bool solution(vector<string> phone_book) {
     int n = phone_book.size();
     sort(phone_book.begin(),  phone_book.end());
-    
+
     for (int i=0; i<n-1; i++) {
         if (phone_book[i+1].substr(0, phone_book[i].length()) == phone_book[i]) return false;
     }
-    
+
     return true;
 }
