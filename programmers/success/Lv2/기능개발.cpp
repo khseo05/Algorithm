a004f75235f42746280d90f6dae2c8de1eabdb87
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-05-27 05:37:12 +0000

    success / 기능개발

--- programmers/success/Lv2/기능개발.cpp
+++ programmers/success/Lv2/기능개발.cpp
@@ -8,21 +8,21 @@ using namespace std;
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> day;
-    
+
     for (int  i=0; i<progresses.size(); i++) {
         day.push_back(ceil((100.0 - progresses[i])/speeds[i]));
     }
-    
+
     int i = 0;
     while (i < progresses.size()) {
         int cnt = 0;
         while (i + cnt < progresses.size() && day[i+cnt] <= day[i]) {
             cnt++;
         }
-        
+
         answer.push_back(cnt);
         i += cnt;
     }
-    
+
     return answer;
 }
