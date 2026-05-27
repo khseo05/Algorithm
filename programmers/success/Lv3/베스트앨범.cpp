30f8c9307634860427e186c777636dd07100a989
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-05-27 07:13:41 +0000

    success / 베스트앨범

--- programmers/success/Lv3/베스트앨범.cpp
+++ programmers/success/Lv3/베스트앨범.cpp
@@ -9,23 +9,23 @@ using namespace std;
 struct Node {
     int p;
     int n;
-    
+
     bool operator <(const Node& other) const {
         if (p == other.p) return n < other.n;
         return p > other.p;
-    } 
+    }
 };
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     unordered_map<string, vector<Node>> m;
     unordered_map<string, int> total;
-    
+
     for (int i=0; i<genres.size(); i++) {
         m[genres[i]].push_back({plays[i], i});
         total[genres[i]] += plays[i];
     }
-    
+
     vector<pair<string, int>> v(total.begin(), total.end());
     sort(v.begin(), v.end(), [](auto& a, auto& b) {
         return a.second > b.second;
@@ -37,7 +37,7 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
         for (int i=0; i<min((int)node.size(),  2); i++)  {
             answer.push_back(node[i].n);
         }
-    }    
-    
+    }
+
     return answer;
 }
