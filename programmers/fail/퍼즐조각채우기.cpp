27c5e7cb734e10f6d889625b012be8cd5d98c92d
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-06-04 11:05:02 +0000

    fail / 퍼즐조각채우기

---
+++ programmers/fail/퍼즐조각채우기.cpp
@@ -0,0 +1,10 @@
+//2026.06.04 fail
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> game_board, vector<vector<int>> table) {
+    int answer = -1;
+    return answer;
+}
