dbcdb88c452e0921d876580f7d523afaeeb5b13d
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-06-30 11:22:29 +0000

    success / 두 큐 합 같게 만들기

--- main.cpp
+++ main.cpp
@@ -1,57 +1,42 @@
-//2026.06.30 fail
+//2026.06.30 success
 #include <string>
 #include <vector>
-#include <stack>
+#include <queue>
 
 using namespace std;
 
-void convert(string& str) {
-    if (str.size() == 0) return;
-    
-    string word = str.substr(1, str.size()-2);
-    for (char& c : word) c = (c == '(') ? ')' : '(';
-    
-    str = "(" + word + ")";
-}
-
-bool correct(string str) {
-    int balance = 0;
-    for (char c : str) {
-        if (c == '(') balance++;
-        else balance--;
-        if (balance < 0) return false;
-    }
-    return true;
-}
-
-string solution(string p) {
-    string answer = "";
-    stack<char> open;
-    stack<char> close;
-    string str = "";
-    for (int i=0; i<p.size(); i++) {
-        if (open.size() == close.size() && open.size() != 0) {
-            if (str.front() == '(' && correct(str)) answer += str;
-            else {
-                convert(str);
-                answer += str;
-            }
-            str = "";
-            open = stack<char>();
-            close = stack<char>();
-        }
-        
-        if (p[i] == '(') open.push(p[i]);
-        if (p[i] == ')') close.push(p[i]);
-        str += p[i]; 
+int solution(vector<int> queue1, vector<int> queue2) {
+    long long sum1 = 0;
+    queue<long long> q1;
+    for (auto x : queue1) {
+        sum1 += x;
+        q1.push(x);
     }
     
-    if (!str.empty()) {
-        if (correct(str)) answer += str;
-        else {
-            convert(str);
-            answer += str;
+    long long sum2 = 0;
+    queue<long long> q2;
+    for (auto x : queue2) {
+        sum2 += x;
+        q2.push(x);
+    }
+    
+    for (long long i=0; i<= queue1.size() * 2 + 2; i++) {
+        if (sum1 == sum2) return i;
+        if (sum1 > sum2) {
+            long long val = q1.front();
+            q1.pop();
+            q2.push(val);
+            sum2 += val;
+            sum1 -= val; 
+        } else if (sum1 < sum2) {
+            long long val = q2.front();
+            q2.pop();
+            q1.push(val);
+            sum1 += val;
+            sum2 -= val;
         }
     }
-    return answer;
+    
+    
+    return -1;
 }
